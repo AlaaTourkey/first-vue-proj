a6ddd4d612c8bd30565b596749d1b74262e7ab78
AlaaTourkey <alaatourkey@gmail.com>
2024-07-12 07:51:34 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Home from '@/components/Home.vue';
+import HomeCard from '@/components/HomeCard.vue';
+import JobList from '@/components/JobList.vue';
+</script>
+
+<template>
+
+  <Navbar />
+  <Home title="test props" subTitle="test subtitle"/>
+  <HomeCard/>
+  <JobList :limit="3" :showButton="true"/>
+</template>
+
+<style scoped>
+
+</style>
